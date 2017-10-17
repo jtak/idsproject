7fca8a7a6a2fb76eef298be9bb28f5cf9509a45e
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-10-17 16:01:15 +0300

    komentorivi parametri säätöä

--- csv_exporter.py
+++ csv_exporter.py
@@ -1,24 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+
+def convertExcelToCsv(file):
+    data = pd.read_excel(file, sheetname = "Indeksit")
+    data['Date & Time'] = data['Date & Time'].str.replace('24:00', '23:59')
+    data.replace(["NoData", "OffScan"], np.NaN, inplace=True)
+    
+    data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst=True)
+    data = data.set_index(data["Date & Time"])
+    
+    year = str(data["Date & Time"][1].year)
+    data = data.drop(['Date & Time'],1)
+    
+    data.to_csv("./resources/raw_data" + year +".csv")
+
 args = sys.argv
-file = args[1]
-
-data = pd.read_excel(file, sheetname = "Indeksit")
-data['Date & Time'] = data['Date & Time'].str.replace('24:00', '23:59')
-data.replace(["NoData", "OffScan"], np.NaN, inplace=True)
-
-dn = data['Date & Time'].str.split(' ')
-data['Time'] = [val[1] for val in dn]
-
-
-data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst=True)
-data = data.set_index(data["Date & Time"])
-year = str(data["Date & Time"][0])[0:4]
-data = data.drop(['Date & Time'],1)
-
-data.to_csv("raw_data" + year +".csv")
-
-
+if(len(args) == 2):
+    convertExcelToCsv(args[1])
+else:
+    for i in range(2014,2018):
+        file = './resources/excel-files/Ilmanlaatuindeksit' + str(i) + '.xls'
+        if(i == 2016):
+            file = './resources/excel-files/Ilmanlaatuindeksit' + str(i) + '.xlsx'
+        convertExcelToCsv(file)
--- day_averages.py
+++ day_averages.py
@@ -2,14 +2,22 @@ import pandas as pd
 import numpy as np
 import sys
 
+def dayAverages(file):
+    data = pd.read_csv(file)
+    
+    data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst=True)
+    data = data.set_index(data["Date & Time"])
+    
+    year = str(data["Date & Time"][1].year)
+    
+    data = data.resample('D').mean()
+    data.to_csv('./resources/daily_averages' + year +'.csv')
+    
+
 args = sys.argv
-
-file = args[1]
-
-data = pd.read_csv(file)
-
-data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst=True)
-data = data.set_index(data["Date & Time"])
-year = str(data["Date & Time"][1].year)
-data = data.resample('D').mean()
-data.to_csv('./resources/daily_averages' + year +'.csv')
+if(len(args) == 2):
+    dayAverages(args[1])
+else:
+    for i in range(2014,2018):
+        file = './resources/raw_data' + str(i)+ '.csv'
+        dayAverages(file)
