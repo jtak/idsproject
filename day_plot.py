f043785684ee125b9eb5d26e50e26787f3626c48
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-10-19 17:15:16 +0300

    kuvasäätöä

--- day_plot.py
+++ day_plot.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = 0.8
 
 data = pd.read_csv('./resources/daily_averages2014.csv')
-plt.plot(data['Mannerheimintie'], color = 'red')
-plt.show()
+im =plt.imread('Seutukartta3.png')
+plt.imshow(im)
+
+colors = ['g','y', 'o', 'r', 'p']
+plt.scatter([780],[800], c= 'r' )
+plt.axis('off')
+plt.savefig("test.png", transparent = True, bbox_inches = 'tight', pad_inches = 0, dpi= 600)
