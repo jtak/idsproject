4020245984f5887a5332308c0f8f840b362505d8
Jani Takkinen <jani.takkinen@cs.helsinki.fi>
2017-10-25 18:42:19 +0300

    arrrrrrrrgh

--- plottingtrafficandairquality.py
+++ plottingtrafficandairquality.py
@@ -7,63 +7,64 @@ import numpy as np
 data14 = pd.read_csv('./resources/month_average_weeks2014.csv')
 data15 = pd.read_csv('./resources/month_average_weeks2015.csv')
 traffic = pd.read_csv('./resources/traffic_columns.csv')
-hesari = traffic['HELSINGINKATU']
-def plot_traffic_and_aq(month, place):    
-    place = data15[data15['Date & Time'] == month][place]
+
+def plot_traffic_and_aq(month, placename):    
+    placetraffic = traffic[placename]
+    place = data15[data15['Date & Time'] == month][placename]
+    
+    """
     host = host_subplot(111, axes_class=AA.Axes)
-    #plt.subplots_adjust(right=0.75)
+    plt.subplots_adjust(right=0.75)
     
     par1 = host.twinx()
-    #par2 = host.twinx()
-    
-    #offset = 60
-    #new_fixed_axis = par2.get_grid_helper().new_fixed_axis
-    #par2.axis["right"] = new_fixed_axis(loc="right", axes=par2, offset=(offset, 0))
-    
-    #par2.axis["right"].toggle(all=True)
-
+   
     host.set_xlim(0, 25,1)
-    
+    """
+    fig, ax1 = plt.subplots()
     
     for weekday in range(0, 5):
         placedata = place.iloc[24*weekday:24*(weekday+1)]
-        #host.set_ylim(0, np.round(np.max(kalliodata.values), decimals = 0) +10)
+        ax1.plot(list(range(0,24)), placedata)
+        #p1, = host.plot(list(range(24)), placedata)
+       
         
-        #par2.set_ylabel("Velocity")
-        
-        p1, = host.plot(list(range(24)), placedata)
-        
-        #p3, = par2.plot([0, 1, 2], [50, 30, 15], label="Velocity")
-        
-        #par1.set_ylim(0, np.round(np.max(hesari.values)))
-        #par2.set_ylim(1, 65)
-        
-        #host.legend()
     
-    host.set_xlabel("Hour")
-    host.set_ylabel("Air quality ")
-    par1.set_ylabel("Cars")
-    host.set_ylim(0, np.round(np.max(place.values), decimals = 0) +10)
-    p2, = par1.bar(left= list(range(24)), height= hesari[:24], label="Cars", alpha = 0.5)
+    #Oli host
+    ax1.set_xlabel("Hour")
+    ax1.set_ylabel("Air quality ")
     
-    host.axis["left"].label.set_color(p1.get_color())
-    par1.axis["right"].label.set_color(p2.get_color())
-        
-        
-    #par2.axis["right"].label.set_color(p3.get_color())
+    ax2 = ax1.twinx()
+    ax2.bar(left= list(range(24)), height=placetraffic[:24], label="Cars", alpha = 0.5)
+    
+    ax2.set_ylabel("Cars")
+    
+    #par1.set_ylabel("Cars")
+    ax1.set_ylim(0, np.round(np.max(place.values), decimals = 0) +10)
+    kuva = placename + str(month)
+    plt.savefig(kuva)
+    
+    #plt.savefig("saatana" + place + str(month) + ".png")
+    #par1.bar(left= list(range(24)), height=placetraffic[:24], label="Cars", alpha = 0.5)
+    
+    #host.axis["left"].label.set_color(p1.get_color())
+    #par1.axis["right"].label.set_color(p2.get_color())
+    
+#for mesta in ['Mäkelänkatu2', 'Kallio2', 'Mannerheimintie']:
+#    for kuukausi in [1, 3, 6, 9]:
 
+plot_traffic_and_aq(3, 'Mannerheimintie')
+plot_traffic_and_aq(8, 'Mannerheimintie')
 
-plot_traffic_and_aq(9, 'Kallio2')
 
 '''
 for val in range(0,1):
-    plt.bar(left= range(0,24,1), height =(hesari[val*24:24*(val+1)]).reset_index(drop = True), color=cols[val], alpha=0.5)
+    plt.bar(left= range(0,24,1), height =(placetraffic[val*24:24*(val+1)]).reset_index(drop = True), color=cols[val], alpha=0.5)
     plt.text(val*5,(3600),str(2014+val), size = 20, color = cols[val])
     
 
 
 for val in range(7):
     plotdata = kallio.iloc[24*val:24*(val+1)]
-    plt.bar(left = plotdata.index[24*val:24*(val+1)], height = hesari[:24]/10, alpha = 0.5)
+    plt.bar(left = plotdata.index[24*val:24*(val+1)], height = placetraffic[:24]/10, alpha = 0.5)
     plt.plot(plotdata)
 '''
