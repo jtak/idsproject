4a415df6ad71bf281e17d071af300f798f8821ac
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-10-25 14:03:42 +0300

    month's average weeks

---
+++ average_weeks.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import sys
+    
+def weekAverageByMonth(year):
+    file = './resources/raw_data' + str(year) +'.csv'
+    data = pd.read_csv(file)
+    data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst = True)
+    month_avg = data.groupby([data['Date & Time'].dt.month,data['Date & Time'].dt.weekday,data['Date & Time'].dt.hour]).mean()
+    
+    month_avg.to_csv('./resources/month_average_weeks' + str(year) + '.csv')
+
+args = sys.argv
+
+if(len(args) == 2):
+    weekAverageByMonth(args[1])
+else:
+    for year in range(2014,2018):
+        weekAverageByMonth(year)
