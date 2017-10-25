e1edaa96eebd296298e3a19767607b14e3cda872
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-10-25 17:09:25 +0300

    avg_workdays

--- average_weeks.py
+++ average_weeks.py
@@ -4,15 +4,21 @@ import sys
     
 def weekAverageByMonth(year):
     file = './resources/raw_data' + str(year) +'.csv'
-    #file = './resources/raw_data2015.csv'
     data = pd.read_csv(file)
     data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst = True)
+    data = data[data['Date & Time'].dt.weekday < 5]
     month_avg = data.groupby([data['Date & Time'].dt.month,data['Date & Time'].dt.weekday,data['Date & Time'].dt.hour]).mean()
-    
     month_avg.to_csv('./resources/month_average_weeks' + str(year) + '.csv')
 
 args = sys.argv
 
+"""file = './resources/raw_data2015.csv'
+data = pd.read_csv(file)
+data["Date & Time"] = pd.to_datetime(data["Date & Time"], dayfirst = True)
+data = data[data['Date & Time'].dt.weekday < 5]
+month_avg = data.groupby([data['Date & Time'].dt.month,data['Date & Time'].dt.weekday,data['Date & Time'].dt.hour]).mean()
+#month_avg.index.rename(['m','d','h'], inplace = True)
+#month_avg.reset_index()"""
 if(len(args) == 2):
     weekAverageByMonth(args[1])
 else:
